7eecc5498c052a67e07a073683741b81efa8323f
Akmaral Tursunbaeva <157477883+akmaral2011@users.noreply.github.com>
2025-12-06 22:37:50 +0600

    Time: 49 ms (36.5%), Space: 54.2 MB (38.66%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,9 +4,10 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    let result = [];
-    for(i=0;i<arr.length;i++){
-        result.push(fn(arr[i],i))
-    }
-    return result
+    return arr.map(fn)
+    // let result = [];
+    // for(i=0;i<arr.length;i++){
+    //     result.push(fn(arr[i],i))
+    // }
+    // return result
 };
