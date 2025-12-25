e6ff984ddbeeb22762b97252770317b2d8f6ffbb
Akmaral Tursunbaeva <157477883+akmaral2011@users.noreply.github.com>
2025-12-25 14:50:23 +0600

    Time: 0 ms (100%), Space: 58.3 MB (41.22%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    let result=[];
+        for(let i=0;i<nums.length;i++){
+            result.push(nums[i])
+        }
+        return [...nums,...result]
+    
+};
