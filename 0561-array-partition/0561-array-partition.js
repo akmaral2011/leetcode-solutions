8bae8dd315696239f5de1f4446145a1d3b786c52
Akmaral Tursunbaeva <157477883+akmaral2011@users.noreply.github.com>
2026-04-06 13:21:22 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -3,11 +3,13 @@
  * @return {number}
  */
 var arrayPairSum = function(nums) {
-    let array = nums.sort((a,b)=>a-b)
-    sum = 0 
-    for(let i=0;i<array.length;i+=2){
+    nums.sort((a,b)=>a-b)
+    let sum = 0 
+    for(let i=0;i<nums.length;i+=2){
        sum+=nums[i]
         
         }
        return sum
+
+       // return nums.sort((a,b)=>a-b).filter((_,index)=>index%2===0).reduce((a,b)=>a+b)
 };
